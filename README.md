b15a4e925b59a5c6f7f6416825882ff72e19de52
Vy Nguyen <vynguyen205@Vys-MacBook-Air.attlocal.net>
2022-05-15 14:30:39 -0700

    changed datr format and added info to the left side of page

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Weather-Dashboard
 homework-6
+https://www.w3schools.com/js/js_if_else.asp
+
+https://tecadmin.net/get-current-date-time-javascript/
+
+https://openweathermap.org/current
--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,26 @@ var oneCallApi = `https://api.openweathermap.org/data/2.5/onecall`;
 var geoApi = `http://api.openweathermap.org/geo/1.0/direct`;
 var lat;
 var lon;
+var today = new Date ();
+//getHours() -- current hour between 0-23
+var hour = today.getHours();
+//getMinutes() -- current minutes between 0-59
+var minute = today.getMinutes()
+var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-//DOM Element 
+//DOM Element Search
 var searchForm = document.querySelector("#searchForm");
 var searchText = document.querySelector("#searchText");
 var searchBtn = document.querySelector(".searchBtn");
-//DOM Element <BODY>
+//DOM Element left
+var currentTime = document.querySelector(".currentTime")
+var dayAndDt = document.querySelector(".dayAndDt");
+var greeting = document.querySelector(".greeting");
+var eachCard = document.querySelector(".eachCard");
+var dailyWeatherIcon = document.querySelector(".dailyWeatherIcon");
+var dailyDate = document.querySelector(".dailyDt");
+var dailyTemp = document.querySelector(".dailyTemp");
+//DOM Element <BODY> right
 var weatherContainer = document.querySelector(".weatherContainer");
 var CTName = document.querySelector(".cityName");
 var todaysDate = document.querySelector(".todayDate");
@@ -30,7 +44,24 @@ var weatherCards = document.querySelector(".mainWeatherCards");
 //Openweather API key
 const apiKey = "b256ae4b79d834242cabefb17b1c0012";
 
+function getTime () {
+    var time = (hour - 12) + ":" + minute;
+    
+    //conditional statement for am and pm, and greetings
+    if (time < 12){
+        currentTime.innerHTML = time + "AM";
+        greeting.textContent = "Good Morning!"
 
+    //current time is 6pm or greater, greet evening
+    } else if (time >= 18) {
+        greeting.textContent = "Good evening!"
+    } else {
+        currentTime.innerHTML = time + "PM";
+        greeting.textContent = "Good Afternoon!"
+    }
+
+}
+getTime();
 
 //function to displayed current weather data when searched for city, zip or country code
 function getLocationWeather(city) {
@@ -50,12 +81,12 @@ function getLocationWeather(city) {
         getWeatherInfo(lat, lon)
         //display city name
         CTName.textContent = city.toUpperCase();
-
     })
 
 } 
-
+//default Location
 getLocationWeather("Irvine")
+
 
 // one call Fecth
 function getWeatherInfo (latitude, longitude){
@@ -68,8 +99,8 @@ function getWeatherInfo (latitude, longitude){
 
         //get date
         var UTCDate = data.current.dt;
-        var locationDate = new Date((UTCDate*1000));
-        var formatedDate = locationDate.toLocaleDateString();
+        var locationDate = new Date(UTCDate * 1000)
+        var formatedDate = days[locationDate.getDay()];
             todaysDate.textContent= formatedDate;
 
         //set vars for weather Information
@@ -81,19 +112,37 @@ function getWeatherInfo (latitude, longitude){
             feelsLike.textContent = `Feels Like | ${weatherFeels} °F`;
         var weatherDescription = data.current.weather[0].description;
             description.textContent = weatherDescription;
-        // var weatherClouds = data.current.clouds;
-        //     clouds.textContent = weatherClouds
-        // var weatherVisibility = data.current.visibility;
-        //     visibility.textContent = `Visibility: ${weatherVisibility}`;
         var weatherHumid = data.current.humidity;
-        humidity.textContent = `Hum | ${weatherHumid} %`;
+            humidity.textContent = `Hum | ${weatherHumid} %`;
         var weatherUV = Math.round(data.current.uvi * 10) / 10;
-        UVIndex.textContent = `UV | ${weatherUV} %`;
-        
+            UVIndex.textContent = `UV | ${weatherUV} %`;
+
+        //loop through daily forecast
+        for (var d = 0; d < 8; d++){
+
+            var dailyDt = data.daily[d].dt;
+            var dailyDtConvert = new Date(dailyDt * 1000);
+            var dailyDtFormat = days[dailyDtConvert.getDay()];
+
+            var dailyWeather = data.daily[d].temp.max;
+
+            var dailyIcon = data.daily[d].weather[0].icon;
+
+            let output = /*html*/ `
+                <div class="eachCard">
+                    <img class="dailyWeatherIcon"  src= "http://openweathermap.org/img/wn/${dailyIcon}@2x.png"alt= "Weather Icon">
+                    <div class="dailyDt">${dailyDtFormat}</div>
+                    <div class="dailyTemp">${dailyWeather} °F</div>
+                </div>
+            `;
+            $('#dailyForecast').html(output);
+        }       
+
     })
 }
 
 //filter search results and save it 
+
 
 function searchCity(event) {
     event.preventDefault();
@@ -121,15 +170,15 @@ function searchCity(event) {
 
 //adding to local storage
 
-// function displayStorage (){
+function displayStorage (){
     
-//      var displayHistory= JSON.parse(localStorage.getItem("city")) || [];
+     var displayHistory= JSON.parse(localStorage.getItem("city")) || [];
 
-//      if (displayHistory) {
+     if (displayHistory) {
 
-//      }
+     }
 
     
-// }
+}
 
 searchBtn.addEventListener("click", searchCity);
